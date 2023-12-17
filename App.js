5b8e7fe27c364f1e9720770ecb5e0ad6de0192b4
Toissincera <uzmanqaisar12@gmail.com>
2023-12-17 13:32:48 +0530

    first big chungus - now up to date

--- App.js
+++ App.js
@@ -1,14 +1,143 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+function App() {
+  const totalContacts = 420;
+  const recentList = [
+    {name: 'Ghostbusters', number: '9876543210'},
+    {name: 'A Friend', number: '1800 69 420'},
+    {name: 'Your Bluff Mr. Bond', number: 'AAA6687654'},
+    {name: 'Ghostbusters', number: '9876543210'},
+    {name: 'A Friend', number: '1800 69 420'},
+    {name: 'Your Bluff Mr. Bond', number: 'AAA6687654'},
+    {name: 'Ghostbusters', number: '9876543210'},
+    {name: 'A Friend', number: '1800 69 420'},
+    {name: 'Your Bluff Mr. Bond', number: 'AAA6687654'},
+    {name: 'Ghostbusters', number: '9876543210'},
+    {name: 'A Friend', number: '1800 69 420'},
+    {name: 'Your Bluff Mr. Bond', number: 'AAA6687654'},
+  ];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'salmon',
+        height: '100%', 
+        width: '100%',
+      }}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'linen',
+          height: '100%',
+          width: '100%',
+        }}>
+        <View
+          style={{
+            height: '7%',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            paddingHorizontal: 36,
+            backgroundColor: 'thistle',
+          }}>
+          <View
+            style={{
+              width: '30%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            {/* <Text
+              style={{
+                color: 'blue',
+                fontSize: 24,
+              }}>
+              📞
+            </Text> */} 
+            {/* <Icon name="heart" type="font-awesome" /> */}
+          </View>
+          <View
+            style={{
+              width: '30%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{
+                color: 'blue',
+                fontSize: 24,
+              }}>
+              🧑‍🦰
+            </Text>
+          </View>
+          <View
+            style={{
+              width: '30%',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{
+                color: 'blue',
+                fontSize: 24,
+              }}>
+              ✔️
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: '1.5%',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            paddingHorizontal: 36,
+            backgroundColor: 'tomato',
+          }}></View>
+        <View
+          style={{
+            height: '6.5%',
+            width: '90%',
+            marginHorizontal: '5%',
+            borderRadius: 36,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            backgroundColor: 'gainsboro',
+          }}>
+          <TextInput
+            style={{
+              fontSize: 20,
+              width: '80%',
+            }}
+            placeholder={`🔍  ${totalContacts} contacts`}></TextInput>
+        </View>
+        <View
+          style={{
+            height: '1.5%',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            paddingHorizontal: 36,
+            backgroundColor: 'tomato',
+          }}></View>
+        <ScrollView style={{height: '83.5%', backgroundColor: 'grey'}}>
+          {recentList.map((item, index) => (
+            <View
+              key={index}
+              style={{
+                padding: 20,
+                backgroundColor: 'lightgray',
+                borderWidth: 1,
+                height: "11.5%"
+              }}>
+              <Text>{item.name} </Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
+
+export default App;
+
 
 const styles = StyleSheet.create({
   container: {
